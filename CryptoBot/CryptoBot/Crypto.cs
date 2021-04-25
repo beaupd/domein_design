98d1114294327690e363da5f4eb2047eaac75abb
beaupd <beau@patrickdekker.nl>
2021-04-25 20:29:32 +0200

    Crypto class

---
+++ CryptoBot/CryptoBot/Crypto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoBot
+{
+    class Crypto : Coin
+    {
+        private string networkName;
+        private double amount;
+
+        public Crypto(string name, double amount, double worth, string networkName)
+            : base(name, amount, worth)
+        {
+            this.networkName = networkName;
+            this.amount = amount;
+        }
+
+        public string getNetworkName()
+        {
+            return this.networkName;
+        }
+
+        public string toString()
+        {
+            return $"{this.name} is on the {this.networkName} network and is worth {this.amount}";
+        }
+    }
+}
