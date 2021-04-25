86a4fc832c4e38b386cc1f79ad8449129f747c9b
beaupd <beau@patrickdekker.nl>
2021-04-25 20:28:57 +0200

    Coin functie

---
+++ CryptoBot/CryptoBot/Coin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoBot
+{
+    public class Coin
+    {
+        public string name;
+        public double amount;
+        public double worth;
+        public Coin(string nm, double amnt, double wrth)
+        {
+            this.name = nm;
+            this.amount = amnt;
+            this.worth = wrth;
+        }
+        public void addAmount(double amount)
+        {
+            this.amount += amount;
+        }
+
+        public bool subAmount(double amount)
+        {
+            if (this.amount - amount > 0)
+            {
+                this.amount -= amount;
+                Console.WriteLine($"Sufficient balance");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient balance");
+                return false;
+            }
+        }
+        
+        public void newWorth(double worth)
+        {
+            this.worth = worth;
+        }
+        
+    }
+}
