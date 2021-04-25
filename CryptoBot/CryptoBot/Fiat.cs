589aa4c17d0a8b441ba76964d07b88890bb7b6a1
beaupd <beau@patrickdekker.nl>
2021-04-25 20:29:57 +0200

    Fiat class

---
+++ CryptoBot/CryptoBot/Fiat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoBot
+{
+    class Fiat : Coin
+    {
+        private string regulator;
+
+        public Fiat(string name, double amount, double worth, string regulator)
+            : base(name, amount, worth)
+        {
+            this.regulator = regulator;
+        }
+
+        public string getRegulator()
+        {
+            return this.regulator;
+        }
+
+        public string toString()
+        {
+            return $"{this.name} is regulated by {this.regulator} and is worth {this.worth}";
+        }
+    }
+}
