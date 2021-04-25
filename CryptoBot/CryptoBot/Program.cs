6b5d4502b250cfa7ff2fffdb00c05adf598f8662
beaupd <beau@patrickdekker.nl>
2021-04-25 20:31:46 +0200

    Program example

--- CryptoBot/CryptoBot/Program.cs
+++ CryptoBot/CryptoBot/Program.cs
@@ -6,7 +6,27 @@ namespace CryptoBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Fiat Euro = new Fiat("Euro", 0.0, 1, "European National banks");
+            Crypto Bitcoin = new Crypto("Bitcoin", 0, 50000, "Bitcoin Network");
+            Crypto Ethereum = new Crypto("Ethereum", 0, 1800, "ERC-20");
+
+            Wallet myWallet = new Wallet("Beau Dekker", Euro);
+
+            myWallet.addFiat(10000);
+            myWallet.addAsset(Bitcoin);
+            myWallet.addAsset(Ethereum);
+
+            Console.WriteLine(myWallet.toString());
+
+            Bot bot = new Bot(myWallet, Ethereum, Euro, "Momentum 4MA with trailstop", 100);
+            bot.buy();
+            bot.buy();
+
+            Ethereum.newWorth(2000);
+
+            bot.sell(200);
+
+            Console.WriteLine(myWallet.toString());
         }
     }
 }
