f1b9ff674c58b1c77293373c9cd35fcea244896e
beaupd <beau@patrickdekker.nl>
2021-04-25 20:30:25 +0200

    Wallet class

---
+++ CryptoBot/CryptoBot/Wallet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoBot
+{
+    class Wallet
+    {
+        public List<Crypto> assets = new List<Crypto>();
+        public Fiat currency;
+        private string walletOwner;
+
+        public Wallet(string walletOwner, Fiat fiat)
+        {
+            this.walletOwner = walletOwner;
+            this.currency = fiat;
+        }
+        public void addAsset(Crypto asset)
+        {
+            this.assets.Add(asset);
+            
+        }
+        public void addFiat(double fiat)
+        {
+            this.currency.amount += fiat;
+
+        }
+
+        public double getTotal()
+        {
+            double total = 0;
+            foreach (Coin coin in assets)
+            {
+                total += coin.worth * coin.amount;
+            }
+            return total;
+        }
+
+        public string toString()
+        {
+            string returnString = "";
+            foreach (Coin coin in assets)
+            {
+                returnString += $"Holding {coin.name}, total amount: {coin.amount}\n";
+            }
+
+            returnString += $"Fiat holdings {getTotal().ToString()}";
+            return returnString;
+        }
+    }
+}
